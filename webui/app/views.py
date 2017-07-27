52876059d5b696904a5f6dac79e2acdd3ed6547b
dgaidukov <d.gaidukov@scalr.com>
2017-07-27 12:32:20 +0300

    refactoring + docx support

--- webui/app/views.py
+++ webui/app/views.py
@@ -1,3 +1,4 @@
+import os
 import shlex
 import subprocess
 import tempfile
@@ -18,71 +19,111 @@ class IndexView(MethodView):
         return render_template('index.html')
 
     def post(self):
+        # check if used uploaded an excel file
         file = request.files['file']
         if file and '.xls' not in file.filename:
             flash("Uploaded file is not an .xls or .xlsx file", "error")
             return redirect(url_for('index'))
 
-        extension = file.filename.split('.')[-1]
-        file_path = tempfile.mktemp(suffix='.' + extension)
-        file.save(file_path)
+        # build dict for HTML conversions for future code simplicity
+        converters = {
+            'pdf': self._convert_to_pdf,
+            'doc': self._convert_to_doc
+        }
 
-        language = request.form.get('language', 'English')
-        out_format = request.form.get('format', 'html')
-        to_pdf = False
-        if out_format == 'pdf':
-            out_format = 'html'
-            to_pdf = True
+        # save file to /tmp folder
+        extension = os.path.splitext(file.filename)[1]
+        uploaded_file_path = tempfile.mktemp(suffix=extension)
+        file.save(uploaded_file_path)
 
-        output_file_name = file.filename + '.' + out_format
+        # get output format
+        output_format = request.form.get('format')
+
+        # process output format and mime type for downloading
+        post_process_to = None
+        mime_type = 'application/text'
+        if output_format == 'html':
+            mime_type = 'text/html'
+
+        if output_format in ('pdf', 'doc'):
+            post_process_to = output_format
+            output_format = 'html'
+
+        # convert uploaded file to html
+        output_file_name = file.filename + '.' + output_format
         output_file_path = tempfile.mktemp(suffix=output_file_name)
+        command_line = self._build_pmix_ppp_tool_run_cmd(uploaded_file_path,
+                                                         output_format,
+                                                         output_file_path)
+        _, stderr = self._run_background_process(command_line)
 
+        # if pmix.ppp tool wrote something to stderr, we should show it to user
+        if stderr:
+            flash("STDERR:\n{}".format(stderr), "error")
+            return redirect(url_for('index'))
+
+        # output_file_path now exists and refers to converted html file
+        # located at /tmp folder
+        file_path = output_file_path
+        file_name = output_file_name
+
+        # if output format is PDF or DOC
+        if post_process_to:
+            converter = converters[post_process_to]
+            file_name, file_path, mime_type = converter(output_file_path)
+
+        return send_file(file_path,
+                         as_attachment=True,
+                         mimetype=mime_type,
+                         attachment_filename=file_name)
+
+    def _convert_to_pdf(self, file_path):
+        pdf_file_path = file_path.replace('.html', '.pdf')
+        command_line = " ".join((
+            config.wkhtmltopdf_executable,
+            file_path,
+            pdf_file_path
+        ))
+        self._run_background_process(command_line)
+        _, pdf_file_name = os.path.split(pdf_file_path)
+        mime_type = 'text/pdf'
+        return pdf_file_name, pdf_file_path, mime_type
+
+    def _convert_to_doc(self, file_path):
+        doc_file_path = file_path.replace('.html', '.docx')
+        os.rename(file_path, doc_file_path)
+        _, doc_file_name = os.path.split(doc_file_path)
+        mime_type = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+        return doc_file_name, doc_file_path, mime_type
+
+    def _run_background_process(self, command_line):
+        args = shlex.split(command_line)
+        process = subprocess.Popen(args,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
+        process.wait()
+        stdout = process.stdout.read().decode().strip()
+        stderr = process.stderr.read().decode().strip()
+
+        return stdout, stderr
+
+    def _build_pmix_ppp_tool_run_cmd(self, in_file_path, out_format,
+                                     out_file_path):
+
+        language = request.form.get('language')
         preset = request.form.get('preset', 'developer')
-
         options = request.form.getlist('options')
-
         if preset != 'custom':
             options = ['preset ' + preset]
 
         command_line = " ".join((
             config.python_executable,
             '-m pmix.ppp',
-            file_path,
+            in_file_path,
             "-l " + language,
             "-f " + out_format,
             *('--{}'.format(option) for option in options),
-            "-o " + output_file_path
+            "-o " + out_file_path
         ))
 
-        args = shlex.split(command_line)
-        p = subprocess.Popen(args,
-                             stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE)
-        p.wait()
-        stdout = p.stdout.read().decode().strip()
-        stderr = p.stderr.read().decode().strip()
-
-        if stdout or stderr:
-            flash("STDOUT:\n{}\nSTDERR:\n{}".format(stdout, stderr), "error")
-            return redirect(url_for('index'))
-
-        if to_pdf:
-            output_file_name = output_file_name.replace('.html', '.pdf')
-            pdf_file_path = output_file_path.replace('.html', '.pdf')
-            command_line = " ".join((
-                config.wkhtmltopdf_executable,
-                output_file_path,
-                pdf_file_path
-            ))
-
-            args = shlex.split(command_line)
-            p = subprocess.Popen(args,
-                                 stdout=subprocess.PIPE,
-                                 stderr=subprocess.PIPE)
-            p.wait()
-            output_file_path = pdf_file_path
-
-        return send_file(output_file_path,
-                         as_attachment=True,
-                         mimetype='text/html' if not to_pdf else 'text/pdf',
-                         attachment_filename=output_file_name)
+        return command_line
